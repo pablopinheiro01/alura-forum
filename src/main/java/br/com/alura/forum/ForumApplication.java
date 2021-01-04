2ece3a17b7d91674be26b2bc1860cce95eb4d3d2
DESKTOP-20POM8C\Pablo <pablo-pinheiro@hotmail.com>
2021-01-04 16:56:54 -0300

    Configuracao de deploy para ambiente de producao e configuracao do projeto para gerar WAR

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -3,6 +3,8 @@ package br.com.alura.forum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -13,10 +15,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport //habilita receber os parametros de paginacao na url
 @EnableCaching
 @EnableSwagger2
-public class ForumApplication {
+//adicionado o SpringBootServletInitializer para gerar um WAR ao invés de um JAR e configurar a subida direto no servidor
+public class ForumApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
+	}
+	
+	//necessario sobreesvrever para subir o WAR
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ForumApplication.class);
 	}
 
 }
